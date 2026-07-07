441ed199ca2f7263c557a3be61ce7f59334e49cd
N1colaOu <n.ouzounov06@gmail.com>
2026-07-07 21:51:10 +0200

    Working implicit heat solver for 2d, i think, should be tested more

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include <Eigen/SparseLU>  
+#include <Eigen/SparseLU>
+#include <Eigen/SparseCholesky>  
 #include <cassert>
 #include <iomanip> // for std::setw
 
@@ -33,7 +34,7 @@ void build_system(SparseMatrix<double>& A, double ht, double hx, double hy, doub
     }
     for (size_t i = 0; i < N; i++) // diag
     {
-        triplets.push_back(Triplet<double>(i, i, 1 + 2/(hx*hx) + 2/(hy*hy)));
+        triplets.push_back(Triplet<double>(i, i, 1 + 2*ht*a/(hx*hx) + 2*ht*a/(hy*hy)));
     }
     for (size_t i = 0; i < N-1; i++) // u diag
     {
@@ -44,38 +45,54 @@ void build_system(SparseMatrix<double>& A, double ht, double hx, double hy, doub
         triplets.push_back(Triplet<double>(i, i + 3, -a*ht/(hy*hy)));
     }
     A.setFromTriplets(triplets.begin(), triplets.end());
+    A.makeCompressed();
 }
 
 
 int main(int argc, char* argv[]){
-    assert(argc == 5);
-
-    const int nx = (int)argv[1][0]-48;
-    const int ny = (int)argv[2][0]-48;
-    const double ht = std::stof(argv[3]);
-    const int t_max = std::stoull(argv[4]);
+    //assert(argc == 5);
+    setNbThreads(1);
+    // const int nx = (int)argv[1][0]-48;
+    // const int ny = (int)argv[2][0]-48;
+    // const double ht = std::stof(argv[3]);
+    // const int t_max = std::stoull(argv[4]);
     //std::cout << t_max << '\n';
 
-    double Lx = 0.50; //m
-    double Ly = 0.50; //m
-    double hx = Lx/nx;
-    double hy = Ly/ny;
-    double a = 1;
+    const int nx = 5;
+    const int ny = 5;
+    const int nt = 15<;
+    const double t_end = 1.00;
+    const double Lx = 1.00; //m
+    const double Ly = 1.00; //m
+    const double hx = Lx/nx;
+    const double hy = Ly/ny;
+    const double ht = t_end/nt;
+    const double a = 0.5;
 
-    VectorXd plate_old(nx*ny);
-    VectorXd plate_cur(nx*ny);
+    VectorXd plate(nx*ny);
     SparseMatrix<double> system{nx*ny, nx*ny};
     
     build_system(system, ht, hx, hy, a, nx*ny);
-    //std::cout << system << '\n';
+    std::cout << system << '\n';
 
-    plate_old(4) = 400; // K 
+    plate(11) = 300.00;
+    plate(17) = 300.00;
+    plate(13) = 300.00;
+    plate(12) = 400.00; // K 
+    plate(7) = 300.00;
 
-    SparseLU<SparseMatrix<double>> solver;
+    SimplicialLLT<SparseMatrix<double>> solver;
     solver.compute(system);
-    plate_cur = solver.solve(plate_old);
+    print_plate(plate, nx, ny);
+    for (size_t i = 0; i < nt; i++)
+    {
+        plate = solver.solve(plate);
+        if(solver.info() != Success){
+            std::cout << "WRONG BOOM!\n";
+            break;
+        }
+        print_plate(plate, nx, ny);
+    }
 
-    print_plate(plate_old, nx, ny);
-    print_plate(plate_cur, nx, ny);
     return 0;
 }
