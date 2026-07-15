0fbb4897725ab83a744f027529dd2920edffdcc3
N1colaOu <n.ouzounov06@gmail.com>
2026-07-15 16:56:07 +0200

    made one common function with coords and editable borders

--- src/setups.h
+++ src/setups.h
@@ -33,68 +33,7 @@ void write_plate(const VectorXd& plate, std::ofstream& file, int nx, int ny){
     file << '\n';
 }
 
-void build_system_0_0_0_0(SparseMatrix<double>& A, double sx, double sy, int nx, int ny){
-
-    const int N = nx*ny;
-    std::vector<Triplet<double>> triplets;
-    triplets.reserve(5*N);
-    for (size_t i = 3; i < N; i++) // ll diag
-    {
-        triplets.push_back(Triplet<double>(i, i - nx, -sy));
-    }
-    for (size_t i = 1; i < N; i++) // l diag
-    {
-        triplets.push_back(Triplet<double>(i, i - 1, -sx));
-    }
-    for (size_t i = 0; i < N; i++) // diag
-    {
-        triplets.push_back(Triplet<double>(i, i, 1 + 2*sx + 2*sy));
-    }
-    for (size_t i = 0; i < N-1; i++) // u diag
-    {
-        triplets.push_back(Triplet<double>(i, i + 1, -sx));
-    }
-    for (size_t i = 0; i < N-3; i++) // uu diag
-    {
-        triplets.push_back(Triplet<double>(i, i + nx, -sy));
-    }
-    A.setFromTriplets(triplets.begin(), triplets.end());
-    A.makeCompressed();
-}
-
-void build_system_0_0_0_0_coords(SparseMatrix<double>& A, double sx, double sy, int nx, int ny, const std::vector<std::pair<double, double>>& coords){
-    
-    const int N = nx*ny;
-    std::vector<Triplet<double>> triplets;
-    triplets.reserve(5*N);
-    // Assembly loops with correct stride and bounds
-    for (size_t i = nx; i < N; i++)          // ll diag
-        triplets.push_back(Triplet<double>(i, i - nx, -sy));
-    for (size_t i = 1; i < N; i++)           // l diag (ideally add i % nx != 0)
-        triplets.push_back(Triplet<double>(i, i - 1, -sx));
-    for (size_t i = 0; i < N; i++)           // main diag
-        triplets.push_back(Triplet<double>(i, i, 1 + 2*sx + 2*sy));
-    for (size_t i = 0; i < N-1; i++)         // u diag (ideally add (i+1) % nx != 0)
-        triplets.push_back(Triplet<double>(i, i + 1, -sx));
-    for (size_t i = 0; i < N - nx; i++)      // uu diag
-        triplets.push_back(Triplet<double>(i, i + nx, -sy));
-
-    
-    
-    A.setFromTriplets(triplets.begin(), triplets.end());
-    for (auto c : coords) {
-        int row = c.first * nx + c.second;   
-        A.coeffRef(row, row) = 1;
-        A.coeffRef(row, row - 1) = 0;
-        A.coeffRef(row, row + 1) = 0;
-        A.coeffRef(row, row - nx) = 0;      
-        A.coeffRef(row, row + nx) = 0;      
-    }
-
-    A.makeCompressed();
-}
-
-void build_system_L_U_R_D(SparseMatrix<double>& A, double sx, double sy, int nx, int ny){
+void build_system_L_U_R_D_coords(SparseMatrix<double>& A, double sx, double sy, int nx, int ny, std::vector<std::pair<double,double>> coords){
 
     int N = nx*ny;
     std::vector<Triplet<double>> triplets;
@@ -128,6 +67,14 @@ void build_system_L_U_R_D(SparseMatrix<double>& A, double sx, double sy, int nx,
     }
 
     A.setFromTriplets(triplets.begin(), triplets.end());
+    for (auto c : coords) {
+        int row = c.first * nx + c.second;   
+        A.coeffRef(row, row) = 1;
+        A.coeffRef(row, row - 1) = 0;
+        A.coeffRef(row, row + 1) = 0;
+        A.coeffRef(row, row - nx) = 0;      
+        A.coeffRef(row, row + nx) = 0;      
+    }
     A.makeCompressed();
 }
 
